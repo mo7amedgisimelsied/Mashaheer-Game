4f7c524203912d464214fd300f4f893c6abdada8
mohamed gisimelsied <mohamed.gisimelsied@iu-study.org>
2024-05-27 01:19:49 +0400

    computer's board added

--- src/Components/ComputerBoard.jsx
+++ src/Components/ComputerBoard.jsx
@@ -1,6 +1,20 @@
 function ComputerBoard(){
+    const chars =   Array(16).fill("?");
     return (
-        <h1>Computer Board</h1>
+        <>
+            <div style={{display: "flex",alignItems: "center",gap: "0.5rem"}}>
+            <img src="https://picsum.photos/30/30" className="computer-profile"/>
+            <p>Computer</p>
+            </div>
+
+            <div className="board">
+            {
+                chars.map(char => <div className="char-card">
+                    {char}
+                </div>)
+            }
+            </div>
+        </>
     )
 }
 
